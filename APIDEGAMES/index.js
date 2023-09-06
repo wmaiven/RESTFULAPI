b1a8e73352a0315d86a892a2b38013010b54300c
wmaiven <maivencunha@hotmail.com>
2023-09-06 12:15:21 -0300

    adicionando db

--- APIDEGAMES/index.js
+++ APIDEGAMES/index.js
@@ -48,10 +48,38 @@ function auth(req, res, next) {
 // Banco de dados em memória (para fins de demonstração)
 var DB = {
     games: [
-        // ...
+        {
+            id: 23,
+            title: "Call of duty MW",
+            year: 2019,
+            price: 60
+        },
+        {
+            id: 65,
+            title: "Sea of thieves",
+            year: 2018,
+            price: 40
+        },
+        {
+            id: 2,
+            title: "Minecraft",
+            year: 2012,
+            price: 20
+        }
     ],
     users: [
-        // ...
+        {
+            id: 1,
+            name: "Victor Lima",
+            email: "victordevtb@guiadoprogramador.com",
+            password: "nodejs<3"
+        },
+        {
+            id: 20,
+            name: "Guilherme",
+            email: "guigg@gmail.com",
+            password: "java123"
+        }
     ]
 }
 
